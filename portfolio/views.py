874815a4cf8a40098e666dd116b1ad777ae88445
Sheridan <ssunier@wellesley.edu>
2014-10-15 15:03:32 -0400

    created a music template

--- portfolio/views.py
+++ portfolio/views.py
@@ -19,8 +19,9 @@ def gallery(request):
 
 def music(request):
   latest_music_list = Music.objects.order_by('-post_date')[:5]
-  output = ', '.join([m.title for m in latest_music_list])
-  return HttpResponse(output)
+  template = loader.get_template('portfolio/music.html')
+  context = RequestContext(request, {'latest_music_list': latest_music_list,})
+  return HttpResponse(template.render(context))
 
 def about(request):
   return HttpResponse("Hello you have found leila's about me page")
